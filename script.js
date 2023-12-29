08a8227441e30a372ff701889d845e10da3cb2fe
Anthony3176 <evelynromero011@gmail.com>
2023-12-28 20:52:18 -0500

    website

--- script.js
+++ script.js
@@ -1,50 +1,10 @@
-// revamp below
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
-// Function that will be called under the scene switcher. 
-function switchToScene(sceneName) {
-    fetch(`${sceneName}.html`)
-    // sceneName would be different html files within the directory (i.e about-me.html). It would automatically add .html at the end.  
-    .then(response => response.text())
-      .then(html => {
-        // Replace the current page content with the loaded scene HTML
-        document.documentElement.innerHTML = html;
-        // Allows for use of back arrow. Saves last page into history. 
-        history.pushState({ scene: sceneName }, '', `?scene=${sceneName}`);
-      })
-       // If an error occurs, throw an error. 
-      .catch(error => {
-        console.error(`Error loading scene: ${error}`);
-      });
-  }
-  
+// Display mobile menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+}
 
-
-////Switching the scene ↓
-//DOMContentLoaded makes sure everything in html file is loaded.
-document.addEventListener('DOMContentLoaded', function() {
-    // Gets list from .menu-select class in index.html
-    const menuSelect = document.querySelector('.menu-select');
-    
-    //Listens to see if a click has happened. 
-    menuSelect.addEventListener('click', function(event) {
-        const target = event.target;
-        //Once a click happens, this ^ and this ↓ work together to figure out what was clicked.
-        if (target.classList.contains('menu-item')) {
-            const buttonText = target.innerText;
-        if (buttonText === 'About me!') {
-          // From here, says it's switching and does the action after the click.
-          console.log("Switching to the About scene...");
-          switchToScene('about-me')
-        } else if (buttonText === 'My Qualities') {
-          console.log("Switching to the Qualities scene...");
-          switchToScene('my-qualities')
-        } else if (buttonText === 'How I spend my free time!') {
-          console.log("Switching to the Free Time scene...");
-          switchToScene('free-time')
-        }
-      }
-    });
-  });
-
-
-      
+menu.addEventListener('click', mobileMenu);
